c7b42fec0742e57e015cc5c97f96288e361f0f91
pola0noch <klayriddell@gmail.com>
2024-12-22 18:25:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/')
-async def root():
-    return {'message': 'Главная страница'}
+async def root() -> str:
+    return 'Главная страница'
 
 @app.get('/user/admin')
-async def admin():
-    return {'message': 'Вы вошли как администратор'}
+async def admin() -> str:
+    return 'Вы вошли как администратор'
 
 @app.get('/user/{user_id}')
-async def user(user_id: int):
-    return {'message': f'Вы вошли как пользователь № {user_id}'}
+async def user(user_id: int) -> str:
+    return f'Вы вошли как пользователь № {user_id}'
 
 @app.get('/user')
-async def user_info(username: str, age: int):
-    return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
+async def user_info(username: str, age: int) -> str:
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
